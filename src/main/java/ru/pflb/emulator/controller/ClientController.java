24c076b461c6e1482ad63bf9447a0ff79d03ea2a
azat-khalilov <68443710+azat-khalilov@users.noreply.github.com>
2020-09-02 23:34:58 +0300

    initCommit

--- src/main/java/ru/pflb/emulator/controller/ClientController.java
+++ src/main/java/ru/pflb/emulator/controller/ClientController.java
@@ -1,9 +1,9 @@
 package ru.pflb.emulator.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 import ru.pflb.emulator.model.dto.ClientDto;
 import ru.pflb.emulator.service.ClientService;
 
@@ -19,7 +19,21 @@ public class ClientController {
     @GetMapping("/client/{id}")
     public ResponseEntity<ClientDto> getHello(@PathVariable long id) {
         ClientDto client = clientService.getUserById(id);
-
         return ResponseEntity.ok(client);
+    }
+    @PostMapping("/client")
+    public ResponseEntity<ClientDto> addCl(@Validated @RequestBody ClientDto client) {
+        return ResponseEntity.status(HttpStatus.OK).build();//{"id":45,"firstName":"Jojo","lastName":"Bean","cardNumber":"123","active":true}
+    }
+
+    @PutMapping("/client/{id}")
+    public ResponseEntity<ClientDto> updCl(@PathVariable long id,@Validated @RequestBody ClientDto client) {
+        //ClientDto client = clientService.getUserById(id);
+        return ResponseEntity.ok(client);
+    }
+
+    @DeleteMapping("/client/{id}")
+    public ResponseEntity<ClientDto> delCl(@PathVariable long id) {
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
--- src/main/java/ru/pflb/emulator/service/impl/ClientServiceImpl.java
+++ src/main/java/ru/pflb/emulator/service/impl/ClientServiceImpl.java
@@ -1,8 +1,14 @@
 package ru.pflb.emulator.service.impl;
 
+import org.apache.coyote.Response;
 import org.springframework.stereotype.Service;
 import ru.pflb.emulator.model.dto.ClientDto;
 import ru.pflb.emulator.service.ClientService;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @Service
 public class ClientServiceImpl implements ClientService {
@@ -10,6 +16,69 @@ public class ClientServiceImpl implements ClientService {
     @Override
     public ClientDto getUserById(long id) {
         //some code
-        return null;
+        Random random=new Random();
+        ClientDto client = ClientDto.builder()
+                .id(id)
+                .firstName(randStr(23,65))
+                .lastName(randStr(23,65))
+                .cardNumber(randStr(10,48))
+                .isActive(randBool())
+                .INN(randStr(10,48))
+                .scanLink(getFilesList().get(random.nextInt(getFilesList().size())))
+                .build();
+        return client;//null;
+    };
+
+
+    public String randStr(int k,int j){
+        StringBuilder s= new StringBuilder("0123456789");
+        for (int i=0;i<10;i++){
+            Random random = new Random();
+            s.setCharAt(i,(char)(random.nextInt(k)+j));
+        }
+        return s.toString();
+    };
+
+    public boolean randBool(){
+        Random random = new Random();
+        if (random.nextInt(100)<50){
+            return true;
+        }else{
+            return false;
+        }
     }
+
+
+
+    public static List<String> getFilesList() {
+
+        final File folder = new File("src/main/resources");
+
+        List<String> result = new ArrayList();
+
+        search(".*", folder, result);
+
+        for (String s : result) {
+            System.out.println(s);
+        }
+        return result;
+    }
+
+    public static void search(final String pattern, final File folder, List<String> result) {
+        for (final File f : folder.listFiles()) {
+
+            if (f.isDirectory()) {
+                search(pattern, f, result);
+            }
+
+            if (f.isFile()) {
+                if (f.getName().matches(pattern)) {
+                    result.add(f.getAbsolutePath());
+                }
+            }
+
+        }
+    }
+
+
 }
